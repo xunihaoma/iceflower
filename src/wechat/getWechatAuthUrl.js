d855e445fa5a3f722d5909f812ee14aefe56ad5c
邹东明 <214695142@qq.com>
2020-07-06 09:36:15 +0800

    iceflower

--- src/wechat/getWechatAuthUrl.js
+++ src/wechat/getWechatAuthUrl.js
@@ -3,9 +3,13 @@
  * url    为授权后的重定向url
  * appId  公众号唯一标识
 */
-var getWeChatAuthUrl = function(url, appId) {
+var getWeChatAuthUrl = function(url, appId, type) {
     var finalUrl = encodeURIComponent(url);
-    var x = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId + '&redirect_uri=' + finalUrl  + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+    var scope = 'snsapi_userinfo';
+    if(type === 'snsapi_base') {
+        scope = 'snsapi_base';
+    }
+    var x = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId + '&redirect_uri=' + finalUrl  + '&response_type=code&scope=' + scope + '&state=STATE#wechat_redirect';
     return x;
 }
 

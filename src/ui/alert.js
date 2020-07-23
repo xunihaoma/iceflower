2fc128f74f3abc2948fbc0cdd3ef41e4ba83c8b2
邹东明 <214695142@qq.com>
2020-07-23 14:50:49 +0800

    iceflower

--- src/ui/alert.js
+++ src/ui/alert.js
@@ -124,7 +124,7 @@ var alert =  function (object) {
         borderRadius: '10px 10px 0 0',
         fontSize: '16px'
     });
-    h3.innerText = o.title || '提示';
+    h3.innerText = object.title || '提示';
     divAlert.appendChild(h3);
 
     var p_text = document.createElement('p');

eab0c878316b9cdb39dda7813a6207e3de2a8bd4
邹东明 <yanzisu168@hotmail.com>
2020-09-11 16:15:25 +0800

    唤起小程序低版本提示

--- src/wechat/miniOpen.js
+++ src/wechat/miniOpen.js
@@ -1,3 +1,4 @@
+var iceAlert = require('../ui/alert.js');
 var miniOpen = function(Vue, object) {
     if (!object) {
         var object = {};
@@ -8,6 +9,22 @@ var miniOpen = function(Vue, object) {
 
     Vue.directive('mini-open', {
         inserted: function(el) {
+            var isWechat = navigator.userAgent.match(/MicroMessenger/i) == 'MicroMessenger' ? true : false;
+            if(!isWechat) {
+                el.addEventListener('click', function() {
+                    iceAlert({text: '请在微信中打开该链接', colorType: 'danger'});
+                })
+                return;
+            }
+            var wechatInfo = navigator.userAgent.match(/MicroMessenger\/([\d\.]+)/i);
+            var wechatVersion = wechatInfo[1].split('.');
+            if( Number(wechatVersion[0]) * 100000 + Number(wechatVersion[1]) * 1000 + Number(wechatVersion[2]) < 700012 ) {
+                el.addEventListener('click', function() {
+                    iceAlert({text: '当前微信版本低于7.0.12 无法唤起小程序', colorType: 'danger'});
+                })
+                return;
+            }
+
             if(!el.style.position) {
                 el.style.position = 'relative';
             }

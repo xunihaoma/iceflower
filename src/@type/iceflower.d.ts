5d46e7a0f3c2c00b3dd5da8e16f66ca44fc2c7d5
邹东明 <2146951422@qq.com>
2020-12-12 17:15:00 +0800

    随便添加一些东西

---
+++ src/@type/iceflower.d.ts
@@ -0,0 +1,45 @@
+// 基本类型
+// ---------------------------------------------------------------
+// number boolean void undefined string symbol null
+// ---------------------------------------------------------------
+
+interface IceflowerAlert {
+    text?: string;
+    confirmBtnText?: string;
+    cancelBtnText?: string;
+    cancelBtnColor?: string;
+    callback?(): void;
+    width?: string;
+    color?: string;
+    zIndex?: number;
+    type?: string;
+    colorType?:  'danger' | 'success' | 'warning';
+    cancel?: boolean;
+    cancelCallback?(): void;
+    close?: boolean;
+    still?: boolean;
+
+}
+
+
+
+
+
+interface Iceflower {
+    alert(object: IceflowerAlert): void;
+    toast(): void;
+    wxShare(): void;
+    checkMobile(): void;
+    urlQuery(name: string): void;
+    loading(): void;
+    closeLoading(): void;
+    getWechatAuthUrl(): void;
+    wxShare(): void;
+    miniOpen(): void;
+    appOpen(): void;
+}
+
+
+
+
+declare var iceflower: Iceflower;
